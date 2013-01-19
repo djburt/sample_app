6a9f400f1e7946f3eb2ca6b66278123bddc5632f
Dan Burt <djburt.78@gmail.com>
2013-01-19 14:09:25 -0500

    Modify routes.rb to define named routes (URI mapping)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  # get "static_pages/home"
+  # get "static_pages/help"
+  # get "static_pages/about"
+  # get "static_pages/contact"
+
+  # URI Mappings
+  # match '/action' automatically creates named routes (e.g. action_path, action_url)
+  # You can have the root of your site routed with "root", just remember to delete public/index.html.
+  root to: 'static_pages#home'                  # match '/', to: 'static_pages#home'
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -53,8 +61,8 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
+  # root :to => "welcome#index"
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
