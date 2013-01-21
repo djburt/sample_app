10af87f0b94308fb99822aeddc5768fa5dad3471
Dan Burt <djburt.78@gmail.com>
2013-01-21 13:34:37 -0500

    Refactor static_pages_spec (2)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,74 +1,44 @@
 require 'spec_helper'
 
-def tag_check(page_path, tag_name, string)
-  visit page_path
-  page.should have_selector(tag_name, :text => string)
-end
-
 describe "StaticPages" do
-
   base_title = "Ruby on Rails Tutorial Sample App"
+  subject { page }
 
   describe "Home Page" do
     before { visit root_path }
     
-    it "should have the h1 'Sample App'" do
-      # tag_check(root_path, 'h1', 'Sample App')
-      page.should have_selector('h1', :text => 'Sample App')
-    end
+    # it "should have the h1 'Sample App'" do
+    #  visit root_path
+    #  page.should have_selector('h1', :text => 'Sample App')
+    # end
+    # = before { visit root_path } + subject { page } +
+    it { should have_selector('h1', :text => 'Sample App') }
 
-    it "should have the base title" do
-      # tag_check(root_path, 'title', base_title)
-      page.should have_selector('title', :text => base_title)
-    end
-
-    it "should not have a custom page title" do
-      # visit root_path
-      page.should_not have_selector('title', :text => '| Home')
-    end
+    it { should have_selector('title', :text => base_title) }
+    it { should_not have_selector('title', :text => '| Home') }
   end
 
   describe "Help Page" do
     before { visit help_path }
 
-    it "should have the h1 'Help'" do
-      # tag_check(help_path, 'h1', 'Help')
-      page.should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the title 'Help'" do
-      # tag_check(help_path, 'title', "#{base_title} | Help")
-      page.should have_selector('title', :text => "#{base_title} | Help")
-    end
+    it { should have_selector('h1', :text => 'Help') }
+    it { should have_selector('title', :text => "#{base_title} | Help") }
   end
 
   describe "About page" do 
     before { visit about_path }
 
-    it "should have the h1 'About Us'" do
-      # tag_check(about_path, 'h1', 'About Us')
-      page.should have_selector('h1', :text => 'About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      # tag_check(about_path, 'title', "#{base_title} | About Us")
-      page.should have_selector('title', :text => "#{base_title} | About Us")
-    end
+    it { should have_selector('h1', :text => 'About Us') }
+    it { should have_selector('title', :text => "#{base_title} | About Us") }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it "should have the h1 'Contact'" do
-      # tag_check(contact_path, 'h1', 'Contact')
-      page.should have_selector('h1', :text => 'Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      # tag_check(contact_path, 'title', "#{base_title} | Contact")
-      page.should have_selector('title', :text => "#{base_title} | Contact")
-    end
+    it { should have_selector('h1', :text => 'Contact') }
+    it { should have_selector('title', :text => "#{base_title} | Contact") }
   end
+
 # Default: rails generate integration_test static_pages
 #  describe "GET /static_pages" do
 #    it "works! (now write some real specs)" do
