54fab2ea088ff15672100e564bc95a1ebb8a795d
Dan Burt <djburt.78@gmail.com>
2013-01-21 14:02:13 -0500

    Add spec/support/utilities.rb with base_title helper method for tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,7 +14,7 @@ describe "StaticPages" do
     # = before { visit root_path } + subject { page } +
     it { should have_selector('h1', :text => 'Sample App') }
 
-    it { should have_selector('title', :text => base_title) }
+    it { should have_selector('title', :text => full_title('')) }
     it { should_not have_selector('title', :text => '| Home') }
   end
 
@@ -22,21 +22,21 @@ describe "StaticPages" do
     before { visit help_path }
 
     it { should have_selector('h1', :text => 'Help') }
-    it { should have_selector('title', :text => "#{base_title} | Help") }
+    it { should have_selector('title', :text => full_title('Help')) }
   end
 
   describe "About page" do 
     before { visit about_path }
 
     it { should have_selector('h1', :text => 'About Us') }
-    it { should have_selector('title', :text => "#{base_title} | About Us") }
+    it { should have_selector('title', :text => full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
     it { should have_selector('h1', :text => 'Contact') }
-    it { should have_selector('title', :text => "#{base_title} | Contact") }
+    it { should have_selector('title', :text => full_title('Contact')) }
   end
 
 # Default: rails generate integration_test static_pages
